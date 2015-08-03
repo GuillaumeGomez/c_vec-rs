8500ab0b5407d8f675987e1040c451640c664a64
Guillaume Gomez <guillaume1.gomez@gmail.com>
2015-08-03 19:47:51 +0200

    Create doc

---
+++ implementors/core/convert/trait.AsMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['c_vec'] = ["impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.AsMut.html' title='core::convert::AsMut'>AsMut</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.slice.html'>[T]</a>&gt; for <a class='struct' href='c_vec/struct.CVec.html' title='c_vec::CVec'>CVec</a>&lt;T&gt;","impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.AsMut.html' title='core::convert::AsMut'>AsMut</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.slice.html'>[T]</a>&gt; for <a class='struct' href='c_vec/struct.CSlice.html' title='c_vec::CSlice'>CSlice</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['c_vec'] = ["impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='c_vec/struct.CVec.html' title='c_vec::CVec'>CVec</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
